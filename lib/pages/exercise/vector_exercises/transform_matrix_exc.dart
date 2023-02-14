50494784c6d9acc393dcdcdd0acc93da6d0cfc7f
Filip Joska <filip@joska.dev>
2023-02-14 21:40:55 +0100

    Update transform matrix exc

--- lib/pages/exercise/vector_exercises/transform_matrix_exc.dart
+++ lib/pages/exercise/vector_exercises/transform_matrix_exc.dart
@@ -1,10 +1,12 @@
 import 'package:dp_algebra/logic/matrix/matrix.dart';
 import 'package:dp_algebra/logic/vector/vector.dart';
 import 'package:dp_algebra/pages/exercise/general/exercise_page.dart';
+import 'package:dp_algebra/utils/exc_utils.dart';
 import 'package:dp_algebra/utils/utils.dart';
 import 'package:dp_algebra/widgets/forms/button_row.dart';
 import 'package:dp_algebra/widgets/input/matrix_input.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_math_fork/flutter_math.dart';
 
 class TransformMatrixExc extends StatefulWidget {
   const TransformMatrixExc({Key? key}) : super(key: key);
@@ -24,21 +26,49 @@ class _TransformMatrixExcState extends State<TransformMatrixExc> {
     return ExercisePage(
       generateButtons: [
         ButtonRowItem(
-          child: const Text('Náhodně'),
-        ),
+            child: const Text('Náhodně'),
+            onPressed: () {
+              setState(() {
+                basisA.clear();
+                basisB.clear();
+
+                int vectorLength = ExerciseUtils.generateSize(min: 2);
+                int vectorCount = ExerciseUtils.generateSize(min: 2);
+
+                // TODO: make sure they are linearly independent
+                for (var i = 0; i < vectorCount; i++) {
+                  basisA.add(
+                    ExerciseUtils.generateVector(length: vectorLength),
+                  );
+                  basisB.add(
+                    ExerciseUtils.generateVector(length: vectorLength),
+                  );
+                }
+              });
+            }),
       ],
       example: Column(
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          // vectors
-          //     .map((v) => Padding(
-          //   padding: const EdgeInsets.symmetric(vertical: 4.0),
-          //   child: Math.tex(
-          //     v.toTeX(),
-          //     textScaleFactor: 1.4,
-          //   ),
-          // ))
-          //     .toList()
+          // TODO: change how it is shown
+          if (basisA.isNotEmpty) const Text('Basis A'),
+          for (var v in basisA)
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 4.0),
+              child: Math.tex(
+                v.toTeX(),
+                textScaleFactor: 1.4,
+              ),
+            ),
+          if (basisB.isNotEmpty) const Text('Basis B'),
+          for (var v in basisB)
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 4.0),
+              child: Math.tex(
+                v.toTeX(),
+                textScaleFactor: 1.4,
+              ),
+            ),
         ],
       ),
       result: MatrixInput(matrix: answer),
