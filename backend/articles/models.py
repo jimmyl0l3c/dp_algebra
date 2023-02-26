a7091cc05669663dcdac79cec8ff1bd48804e1c7
Filip Joska <filip@joska.dev>
2023-02-26 19:47:09 +0100

    Update models

--- backend/articles/models.py
+++ backend/articles/models.py
@@ -5,16 +5,28 @@ class Language(models.Model):
     code = models.CharField(max_length=5, unique=True)
     name = models.CharField(max_length=150)
 
+    def __str__(self):
+        return f'{self.code}'
+
 
 class Chapter(models.Model):
     order = models.BigIntegerField(db_index=True)
+
+    def __str__(self):
+        return f'Chapter {self.id} (order: {self.order})'
+
+    class Meta:
+        ordering = ['order']
 
 
 class ChapterTranslation(models.Model):
     language = models.ForeignKey(Language, on_delete=models.CASCADE)
     chapter = models.ForeignKey(Chapter, on_delete=models.CASCADE)
     title = models.CharField(max_length=150)
-    description = models.CharField(max_length=250)
+    description = models.CharField(max_length=250, blank=True)
+
+    def __str__(self):
+        return f'{self.chapter}/{self.language}: {self.title}'
 
     class Meta:
         db_table = 'articles_chapter_translations'
@@ -25,12 +37,21 @@ class Article(models.Model):
     chapter = models.ForeignKey(Chapter, on_delete=models.CASCADE)
     published_at = models.DateField()
 
+    def __str__(self):
+        return f'Chapter: {self.chapter}, Article: {self.id}, (order: {self.order})'
+
+    class Meta:
+        ordering = ['order']
+
 
 class ArticleTranslation(models.Model):
     language = models.ForeignKey(Language, on_delete=models.CASCADE)
     article = models.ForeignKey(Article, on_delete=models.CASCADE)
     title = models.CharField(max_length=150)
-    description = models.CharField(max_length=250)
+    description = models.CharField(max_length=250, blank=True)
+
+    def __str__(self):
+        return f'{self.article}/{self.language}: {self.title}'
 
     class Meta:
         db_table = 'articles_article_translations'
@@ -40,10 +61,22 @@ class Page(models.Model):
     order = models.BigIntegerField(db_index=True)
     article = models.ForeignKey(Article, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return f'Article: {self.article}, Page: {self.id}, (order: {self.order})'
+
+    class Meta:
+        ordering = ['order']
+
 
 class Block(models.Model):
     order = models.BigIntegerField(db_index=True)
     page = models.ForeignKey(Page, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'Page: {self.page}, Block: {self.id}, (order: {self.order})'
+
+    class Meta:
+        ordering = ['order']
 
 
 class BlockTranslation(models.Model):
