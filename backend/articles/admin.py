ed7a32a944990338d150be036b594625a4ffc368
Filip Joska <filip@joska.dev>
2023-02-26 17:29:51 +0100

    Update models

--- backend/articles/admin.py
+++ backend/articles/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Language, Chapter, ChapterTranslation, Article, ArticleTranslation, Page, Block, BlockTranslation
 
-# Register your models here.
+admin.site.register(Language)
+admin.site.register(Chapter)
+admin.site.register(ChapterTranslation)
+admin.site.register(Article)
+admin.site.register(ArticleTranslation)
+admin.site.register(Page)
+admin.site.register(Block)
+admin.site.register(BlockTranslation)
--- backend/articles/models.py
+++ backend/articles/models.py
@@ -10,11 +10,14 @@ class Chapter(models.Model):
     order = models.BigIntegerField(db_index=True)
 
 
-class ChapterTranslations(models.Model):
+class ChapterTranslation(models.Model):
     language = models.ForeignKey(Language, on_delete=models.CASCADE)
     chapter = models.ForeignKey(Chapter, on_delete=models.CASCADE)
     title = models.CharField(max_length=150)
     description = models.CharField(max_length=250)
+
+    class Meta:
+        db_table = 'articles_chapter_translations'
 
 
 class Article(models.Model):
@@ -23,11 +26,14 @@ class Article(models.Model):
     published_at = models.DateField()
 
 
-class ArticleTranslations(models.Model):
+class ArticleTranslation(models.Model):
     language = models.ForeignKey(Language, on_delete=models.CASCADE)
     article = models.ForeignKey(Article, on_delete=models.CASCADE)
     title = models.CharField(max_length=150)
     description = models.CharField(max_length=250)
+
+    class Meta:
+        db_table = 'articles_article_translations'
 
 
 class Page(models.Model):
@@ -40,8 +46,10 @@ class Block(models.Model):
     page = models.ForeignKey(Page, on_delete=models.CASCADE)
 
 
-class BlockTranslations(models.Model):
+class BlockTranslation(models.Model):
     language = models.ForeignKey(Language, on_delete=models.CASCADE)
     block = models.ForeignKey(Block, on_delete=models.CASCADE)
     content = models.TextField()
 
+    class Meta:
+        db_table = 'articles_block_translations'
