f65892803fb04bb80edff52c0692bc7279cb6894
Filip Joska <filip@joska.dev>
2023-02-26 17:19:46 +0100

    Add articles models

--- backend/algebra_api/urls.py
+++ backend/algebra_api/urls.py
@@ -14,8 +14,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+
+from articles import views
+
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
 
 urlpatterns = [
+    path('', include(router.urls)),
     path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
--- backend/articles/models.py
+++ backend/articles/models.py
@@ -1,3 +1,47 @@
 from django.db import models
 
-# Create your models here.
+
+class Language(models.Model):
+    code = models.CharField(max_length=5, unique=True)
+    name = models.CharField(max_length=150)
+
+
+class Chapter(models.Model):
+    order = models.BigIntegerField(db_index=True)
+
+
+class ChapterTranslations(models.Model):
+    language = models.ForeignKey(Language, on_delete=models.CASCADE)
+    chapter = models.ForeignKey(Chapter, on_delete=models.CASCADE)
+    title = models.CharField(max_length=150)
+    description = models.CharField(max_length=250)
+
+
+class Article(models.Model):
+    order = models.BigIntegerField(db_index=True)
+    chapter = models.ForeignKey(Chapter, on_delete=models.CASCADE)
+    published_at = models.DateField()
+
+
+class ArticleTranslations(models.Model):
+    language = models.ForeignKey(Language, on_delete=models.CASCADE)
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    title = models.CharField(max_length=150)
+    description = models.CharField(max_length=250)
+
+
+class Page(models.Model):
+    order = models.BigIntegerField(db_index=True)
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+
+
+class Block(models.Model):
+    order = models.BigIntegerField(db_index=True)
+    page = models.ForeignKey(Page, on_delete=models.CASCADE)
+
+
+class BlockTranslations(models.Model):
+    language = models.ForeignKey(Language, on_delete=models.CASCADE)
+    block = models.ForeignKey(Block, on_delete=models.CASCADE)
+    content = models.TextField()
+
