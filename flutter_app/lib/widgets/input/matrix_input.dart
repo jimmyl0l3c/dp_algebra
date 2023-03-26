951a6d08d3120cd522913ccdf5329fcd3ca0d0b3
Filip Joska <filip@joska.dev>
2023-03-26 20:08:10 +0200

    Add buttons to remove rows/columns of matrix

--- flutter_app/lib/widgets/input/matrix_input.dart
+++ flutter_app/lib/widgets/input/matrix_input.dart
@@ -29,32 +29,91 @@ class _MatrixInputState extends State<MatrixInput> {
         padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 4.0),
         child: Column(
           children: [
-            if (widget.name != null || widget.deleteMatrix != null)
-              Row(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  if (widget.name != null) Text(widget.name!),
-                  if (widget.deleteMatrix != null)
-                    IconButton(
-                      onPressed: widget.deleteMatrix,
-                      icon: const Icon(Icons.close),
-                      iconSize: 12.0,
-                      splashRadius: 15.0,
-                      color: Colors.redAccent,
+            Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                if (widget.name != null) Text(widget.name!),
+                MenuAnchor(
+                  builder: (context, controller, child) => IconButton(
+                    onPressed: () {
+                      if (controller.isOpen) {
+                        controller.close();
+                      } else {
+                        controller.open();
+                      }
+                    },
+                    iconSize: 15.0,
+                    splashRadius: 18.0,
+                    icon: const Icon(Icons.edit),
+                  ),
+                  menuChildren: [
+                    if (widget.deleteMatrix != null)
+                      MenuItemButton(
+                        leadingIcon: const Icon(
+                          Icons.delete_forever,
+                          color: Colors.redAccent,
+                        ),
+                        onPressed: widget.deleteMatrix,
+                        child: const Text(
+                          'Smazat',
+                          style: TextStyle(
+                            color: Colors.redAccent,
+                          ),
+                        ),
+                      ),
+                    SubmenuButton(
+                      menuChildren: [
+                        if (widget.matrix.rows > 1)
+                          for (var i = 0; i < widget.matrix.rows; i++)
+                            MenuItemButton(
+                              onPressed: () {
+                                setState(() {
+                                  widget.matrix.removeRow(i);
+                                });
+                              },
+                              child: Text('Odebrat $i.'),
+                            ),
+                      ],
+                      child: const Text("Řádek"),
                     ),
-                ],
-              ),
+                    SubmenuButton(
+                      menuChildren: [
+                        if (widget.matrix.columns > 1)
+                          for (var i = 0; i < widget.matrix.columns; i++)
+                            MenuItemButton(
+                              onPressed: () {
+                                setState(() {
+                                  widget.matrix.removeColumn(i);
+                                });
+                              },
+                              child: Text('Odebrat $i.'),
+                            ),
+                      ],
+                      child: const Text("Sloupec"),
+                    ),
+                  ],
+                ),
+                // if (widget.deleteMatrix != null)
+                //   IconButton(
+                //     onPressed: widget.deleteMatrix,
+                //     icon: const Icon(Icons.close),
+                //     iconSize: 12.0,
+                //     splashRadius: 15.0,
+                //     color: Colors.redAccent,
+                //   ),
+              ],
+            ),
             SizedBox(
-              width: 60 * widget.matrix.getColumns().toDouble(),
+              width: 60 * widget.matrix.columns.toDouble(),
               child: GridView.count(
-                crossAxisCount: widget.matrix.getColumns(),
+                crossAxisCount: widget.matrix.columns,
                 shrinkWrap: true,
                 mainAxisSpacing: 0.0,
                 clipBehavior: Clip.antiAlias,
                 crossAxisSpacing: 4.0,
                 children: [
-                  for (var i = 0; i < widget.matrix.getRows(); i++)
-                    for (var j = 0; j < widget.matrix.getColumns(); j++)
+                  for (var i = 0; i < widget.matrix.rows; i++)
+                    for (var j = 0; j < widget.matrix.columns; j++)
                       FractionInput(
                         onChanged: (Fraction? value) {
                           if (value == null) return;
