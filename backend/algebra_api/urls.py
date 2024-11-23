1a6c7c11e4963ad90326044fe7d4adaad951a607
Filip Joska <filip@joska.dev>
2024-11-23 22:25:35 +0100

    Update deps, add browsable API

--- backend/algebra_api/urls.py
+++ backend/algebra_api/urls.py
@@ -7,8 +7,23 @@ The `urlpatterns` list routes URLs to views. For more information please see:
 
 from django.contrib import admin
 from django.urls import include, path
+from rest_framework import permissions
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
+from rest_framework.views import APIView
+
+
+class APIRootView(APIView):
+    permission_classes = (permissions.AllowAny,)
+
+    def get(self, request):
+        return Response({
+            "Learn API v2": reverse("learn-root", request=request),
+        })
+
 
 urlpatterns = [
+    path("", APIRootView.as_view(), name="api-root"),
     path("api/learn/", include("articles.urls")),
     path("learn/", include("articles.v2.urls")),
     path("admin/", admin.site.urls),
